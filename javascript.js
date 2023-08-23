cede2184456a6ea5f4a9b87ac9344145f9f90cb8
YKcasa <ykajiyamawk@gmail.com>
2023-08-23 09:28:46 +0900

    20230822

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
-const btns = document.querySelectorAll(".accordion-btn");
+// アコーディオンメニュー
+const acbtns = document.querySelectorAll(".accordion-btn");
 
-btns.forEach(function(btn) {
+acbtns.forEach(function(btn) {
   btn.addEventListener("click", function() {
     const panel = this.nextElementSibling;
 
@@ -11,3 +12,22 @@ btns.forEach(function(btn) {
     }
   });
 });
+
+// 封筒ボタン制御
+const enbtns = document.querySelectorAll(".envelop-btn");
+// const display = document.getElementById("display-area"); //メインファイルへ
+
+enbtns.forEach(btn => {
+  btn.addEventListener("click", function() {
+    const data1 = buttonDataMap[btn.id][0];
+    const data2 = buttonDataMap[btn.id][1];
+
+    display.textContent = data1 + " x " + data2;
+
+  });
+});
+
+// const buttonDataMap = {
+//   'K1': ['27.0','38.2'],
+//   'K5': ['19.0','24.0']
+// };
