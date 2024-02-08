cac3fd8dc40476fe0e01041da5fea2b7957a2529
YKcasa <131603850+YKcasa@users.noreply.github.com>
2024-02-08 21:26:39 +0900

    1st commit

---
+++ scripts/main.js
@@ -0,0 +1,303 @@
+// --- 表示域との紐づけ
+// 封筒ボタンエリア
+const envelopeArea = document.getElementById('envelope-select');
+// シミュレーションエリア
+const displayPostage = document.getElementById('display-postage');
+const displayMailType = document.getElementById('display-mailtype');
+const displayWeightLimit = document.getElementById('display-weight-limit');
+const displaySize = document.getElementById('display-size-area');
+const displayThickness = document.getElementById('display-thickness-area');
+const displayWeight = document.getElementById('display-weight-area');
+
+// --- 変数定義
+let componentsInstances;
+let inclMaxLong, inclMaxShort, inclTotalThicknes, inclTotalWeight; // 内容物の最大長辺と短辺、内容物全体の厚さと重さ
+let evLongSide, evShortSide, totalThickness, totalWeight; // 封筒の長辺と短辺、封筒込みの厚さと重さ
+let nextWeightLimit;
+
+
+// --- 内容物のクラス
+class Inclusion {
+    constructor(key, label, weight, thickness, type, max, unitName){
+        this.key = key;
+        this.label = label;
+        if (type == 'paper' || type == 'file'){
+            this.basicWeight = weight / (788 * 1091); // 紙の重さ単位：kg/四六判(788mm*1091mm)1000枚　→　g/1mm2　に変換する
+        }else{
+            this.basicWeight = weight;
+        }
+        this.unitWeight = 0;
+        this.basicThickness = thickness;
+        this.foldingFactor = 1; // 二つ折り・三つ折り換算用の係数
+        this.unitThickness = 0;
+        this.materialType = type;
+        this.longSide = 0;
+        this.shortSide = 0;
+        this.quantity = 0;
+        this.quantityMax = max; // ドロップダウンメニューの最大値
+        this.unitName = unitName;
+        this.sizeStandard = sizeStandards[this.materialType] || sizeStandards.default;
+        this.paperSizeSelect = null;
+        this.quantitySelect = null;
+
+        this.createElements(); // インスタンス作成時に画面要素を作成
+        this.attachListeners(); // 作成した画面要素にイベントリスナーを付与
+    }
+
+    createElements() {
+        // 要素を配置するdiv
+        const div = document.createElement('div');
+        div.classList.add('inclusion');
+        document.getElementById('inclusion').appendChild(div);
+
+        // 内容物名称のラベル
+        const label = document.createElement('label');
+        label.textContent = this.label;
+        div.appendChild(label);
+
+        // サイズ選択ドロップダウンメニュー
+        const sizeSelectMenu = document.createElement('select');
+        sizeSelectMenu.id = this.key + '-size';       
+        
+        for (const size in this.sizeStandard) { 
+            const option = document.createElement('option');
+            option.value = size;
+            option.text = size;
+            sizeSelectMenu.appendChild(option);
+        }
+
+        div.appendChild(sizeSelectMenu);
+        this.paperSizeSelect = document.getElementById(sizeSelectMenu.id);
+
+        if (this.materialType == 'others'){
+            this.paperSizeSelect.disabled = true;
+        }
+        
+        // 数量選択ドロップダウンメニュー
+        const quantityInputMenu = document.createElement('select');
+        quantityInputMenu.id = this.key + '-quantity';
+        for (let i = 0; i <= this.quantityMax; i++) {
+            const option = document.createElement('option');
+            option.value = i;
+            option.text = i;
+            quantityInputMenu.appendChild(option);
+        }
+        div.appendChild(quantityInputMenu);
+        this.quantitySelect = document.getElementById(quantityInputMenu.id);
+        
+        // 単位のラベル
+        const unitLabel = document.createElement('label');
+        unitLabel.textContent = this.unitName;
+        div.appendChild(unitLabel);
+    }
+    
+    attachListeners() {
+        this.paperSizeSelect.addEventListener('change', this.updateInclusion.bind(this));
+        this.quantitySelect.addEventListener('change', this.updateInclusion.bind(this));
+    }
+
+    updateInclusion(){ // ドロップダウンの選択内容で内容物のインスタンスを更新する
+        this.clearDisplay();
+
+        const selectedSize = this.paperSizeSelect.value;
+        const quantity = this.quantitySelect.value;
+        
+        this.longSide = this.sizeStandard[selectedSize].long;
+        this.shortSide = this.sizeStandard[selectedSize].short;
+        this.foldingFactor = this.sizeStandard[selectedSize].foldingFactor;
+        
+        this.quantity = Number(quantity);
+        this.unitWeight = Number(this.basicWeight * this.longSide * this.shortSide * this.foldingFactor * this.quantity);
+        this.unitThickness = Number(this.basicThickness * this.foldingFactor * quantity);
+        
+        console.log(this.key, this.quantity, this.unitWeight, this.unitThickness)
+        
+        calculateInclusionTotal();
+        generateEnvelopeButtons();
+        
+        if (selectedSize === 'サイズを選択'){
+            if (quantity !== '0'){
+                this.paperSizeSelect.classList.add('select-emphasis');
+                envelopeArea.innerHTML = '';
+            }
+        }else if (quantity === '0'){
+            if (selectedSize !== 'サイズ指定なし'){
+                this.quantitySelect.classList.add('select-emphasis');
+                envelopeArea.innerHTML = '';
+            }
+        }else{
+            scrollToButtons();
+        }
+    }
+
+    clearDisplay(){ // 表示部分のクリア
+        updateDisplay(displayPostage, '');
+        updateDisplay(displayMailType, '');
+        updateDisplay(displayWeightLimit, '');
+        this.quantitySelect.classList.remove('select-emphasis');
+        this.paperSizeSelect.classList.remove('select-emphasis');
+    }
+}
+
+// --- 内容物の合算 --- 内容物の最大の長辺・短辺、内容物全体の厚さ・重さを計算
+function calculateInclusionTotal() {
+    inclMaxLong = componentsInstances.reduce((max, instance) => Math.max(max, instance.longSide), 0);
+    inclMaxShort = componentsInstances.reduce((max, instance) => Math.max(max, instance.shortSide), 0);
+    
+    inclTotalThicknes = componentsInstances.map(item => item.unitThickness).reduce((sum, curr) => sum + curr, 0);
+    updateDisplay(displayThickness, `${inclTotalThicknes.toFixed(1)} mm`, 500);
+    
+    inclTotalWeight = componentsInstances.map(item => item.unitWeight).reduce((sum, curr) => sum + curr, 0);
+    updateDisplay(displayWeight, `${inclTotalWeight.toFixed(2)} g`, 500);
+    
+    console.log(`長辺:${inclMaxLong}, 短辺:${inclMaxShort}, 厚さ:${inclTotalThicknes}, 重さ:${inclTotalWeight}`);  
+}    
+
+// --- 封筒ボタン生成 --- 内容物が入るサイズの封筒ボタンを表示し、イベントリスナーを設置
+function generateEnvelopeButtons() {
+    envelopeArea.innerHTML = ''; 
+    updateDisplay(displaySize, '');
+
+    Object.keys(envelopeData).forEach(envelopeId => {
+        const data = envelopeData[envelopeId];
+        if(data.long > inclMaxLong && data.short > inclMaxShort){
+            const button = document.createElement("button");
+            button.id = envelopeId;
+            button.textContent = `${data.value}`;
+            button.classList.add('envelope-btn');
+            envelopeArea.appendChild(button);
+        }
+    });
+    
+    const envelopeButtons = document.querySelectorAll('.envelope-btn');
+    envelopeButtons.forEach(btns => btns.addEventListener('click', calculatePostage));
+}
+
+// --- 画面スクロール（封筒ボタンを見せるため。STEP１を画面最上部へ）
+function scrollToButtons(){
+    const scrollTarget = document.getElementById('inclusion');
+    scrollTarget.scrollIntoView({behavior: 'smooth', block: 'start'})
+}
+
+// --- 封筒ボタン選択時　-> 封筒サイズ読み込み、郵便種別判定
+function calculatePostage(){
+    loadEnvelopDatas();
+    postalClassify();
+}
+
+// 封筒サイズ読み込み
+function loadEnvelopDatas(){
+    updateDisplay(displayThickness,'');
+    updateDisplay(displayWeight,'');
+
+    const selectedEnvelopeSize = envelopeData[event.target.id]; //封筒データセットから
+    evLongSide = selectedEnvelopeSize.long; 
+    evShortSide = selectedEnvelopeSize.short;
+    updateDisplay(displaySize, `${evLongSide} mm × ${evShortSide} mm`, 500);
+    
+    totalThickness = inclTotalThicknes + selectedEnvelopeSize.thickness;
+    updateDisplay(displayThickness, `${totalThickness.toFixed(1)} mm`, 500);
+    
+    totalWeight = inclTotalWeight + selectedEnvelopeSize.weight;
+    updateDisplay(displayWeight, `${totalWeight.toFixed(2)} g`, 500);
+    
+    // 計算確認用
+    console.log(`封筒長辺：${evLongSide},封筒短辺：${evShortSide},厚さ：${totalThickness},重さ：${totalWeight}`);
+    // 計算確認用ここまで
+}
+
+// 郵便種別判定 -> 郵便料金判定
+// --- !!! 料金変更の閾値はここにハードコーディングしているので郵便仕様変更時は修正する !!! ---
+function postalClassify() {
+    let mailType, diplayFee;
+    
+    if (totalWeight > 4000){ // 4kg超は取扱い不可。除外
+        mailType = "４ｋｇを超えるものは、手紙ではなくゆうパック等をお使いください";
+    }else if( evLongSide > 600 || (evLongSide + evShortSide + totalThickness) > 900 ){
+        mailType = "９０サイズをオーバーしています。ゆうパック等をお使いください";
+    }else{
+        if(totalWeight > 1000){ // 1kg超は定形外・規格外
+            mailType = `定形外郵便物・規格外`;
+            displayFee = determinePostage(extraPostageArray, totalWeight);
+        }else if(totalWeight > 50){ // 50g超は定形外
+            if (evLongSide > 340 || evShortSide > 250 || totalThickness > 30){
+                mailType = `定形外郵便物・規格外`;
+                displayFee = determinePostage(extraPostageArray, totalWeight);
+            }else{
+                mailType = `定形外郵便物・規格内`;
+                displayFee = determinePostage(nonstandardPostageArray, totalWeight);
+            }   
+        }else{ //50g以内は大きさで３パターン
+            if(evLongSide > 340 || evShortSide > 250 || totalThickness > 30){
+                mailType = `定形外郵便物・規格外`;
+                displayFee = determinePostage(extraPostageArray, totalWeight);
+            }else if(evLongSide > 235 || evShortSide > 120 || totalThickness > 10){
+                mailType = `定形外郵便物・規格内`;
+                displayFee = determinePostage(nonstandardPostageArray, totalWeight);
+            }else{
+                mailType = `定形郵便物`;
+                displayFee = determinePostage(standardPostageArray, totalWeight);
+            };
+        };
+    };
+
+    updateDisplay(displayMailType, mailType, 500);
+    updateDisplay(displayWeightLimit, `あと ${(nextWeightLimit - totalWeight).toFixed(1)}ｇまで同一料金です`, 500);
+    displayPostage.textContent = `${displayFee} 円`;
+    
+}
+
+// 郵便料金判定
+function determinePostage(postalClassArray, weight) {
+    for (i = 0; i < postalClassArray.length; i++){
+        if (weight <= postalClassArray[i].weightLimit){
+            nextWeightLimit = postalClassArray[i].weightLimit;
+            return postalClassArray[i].fee;
+        };
+    }
+}
+
+
+// --- 表示エリアへの表示用共通処理
+function updateDisplay(area, contents, delay = 0){
+    area.animate([{opacity: '0'},{opacity: '1'}], delay);
+    area.textContent = `${contents}`;
+}
+
+function turnPostageCounter(previousValue = 0, targetValue){
+
+    // let displayValue = previousValue;
+
+    // const turnCounter = () => {
+    //     displayPostage.textContent = displayValue;
+    //     displayValue++
+
+    //     if ( > ){
+
+    //     }
+    // }
+
+    // if (previousValue <= targetValue){
+    //     const difference = targetValue - previousValue;
+    //     const speed = 600 / difference;
+    //     clearInterval(counterData);
+    //     counterData = setInterval(turnCounter, speed);
+    // }else{
+    //     const difference = previousValue - targetValue;
+    //     const speed = 600 / difference;
+    //     clearInterval(counterData);
+    //     counterData = setInterval(turnCounter, speed);
+
+    // }
+}
+
+
+// --- 初期化処理　配列データより、インスタンスを作成する
+function initialize() {
+    componentsInstances = inclusions.map(data => new Inclusion(data.id, data.label, data.weight, data.thickness, data.type, data.max, data.unitName));
+}
+
+
+// --- 画面読み込み時に初期化処理を行う
+document.addEventListener('DOMContentLoaded', initialize);
+    
---
+++ scripts/settings.js
@@ -0,0 +1,131 @@
+// --- コンポーネント用の配列
+const inclusions = [
+  {id:'NormalPaper', label:'普通紙 : ', weight:55, thickness:0.12, type:'paper', max: 20, unitName:'枚'},
+  {id:'HeavyPaper', label:'厚紙 : ', weight:90, thickness:0.15, type:'paper', max: 10, unitName:'枚'},
+  {id:'ClearFile', label:'クリアファイル : ', weight:360, thickness:1.5, type:'file', max: 10, unitName:'枚'},
+  {id:'Clips', label:'クリップ : ', weight:1.3, thickness:1.0, type:'others', max: 10, unitName:'個'},
+  // {id:'Dummy', label:'テスト用おもり : ', weight:300, thickness:1.0, type:'others', max: 30, unitName:'枚'}
+  // コンポーネントを適宜追加
+]
+
+// --- サイズ一覧
+const paperSizeStandard = {
+  // 'サイズを選択': {long:0, short:0, foldingFactor:1},
+  'サイズを選択': {long:1, short:1, foldingFactor:1},
+  'A4': {long:297, short:210, foldingFactor:1},
+  'A4（２つ折り）': {long:210, short:148.5, foldingFactor:2},
+  'A4（３つ折り）': {long:210, short:99, foldingFactor:3},
+  'B4': {long:364, short:257, foldingFactor:1},
+  'B4（２つ折り）': {long:257, short:182, foldingFactor:2},
+  'B4（３つ折り）': {long:257, short:121.4, foldingFactor:3},
+  'B4（６つ折り）': {long:182, short:85.7, foldingFactor:6},
+  'A3（２つ折り）': {long:297, short:210, foldingFactor:2},
+  'A3（３つ折り）': {long:297, short:130, foldingFactor:3},
+  'A3（６つ折り）': {long:210, short:99, foldingFactor:6},
+}    
+
+const fileSizeStandard = {
+  // 'サイズを選択': {long:0, short:0, foldingFactor:1},
+  'サイズを選択': {long:1, short:1, foldingFactor:1},
+  'A4': {long:310, short:220, foldingFactor:1},
+  'B4': {long:364, short:257, foldingFactor:1}
+}    
+
+const othersSize = {
+  'サイズ指定なし': {long:1, short:1, foldingFactor:1}
+}    
+
+// --- 内容物のtypeと読み込むサイズ一覧の対応表
+const sizeStandards = {
+  paper: paperSizeStandard,
+  file: fileSizeStandard,
+  default: othersSize
+}
+
+
+// --- 封筒ボタンのデータセット　注：重さは白色ケント紙85kgを想定
+const envelopeData = {
+  'K1': {value:"角型１号", long:382, short:270, thickness:1, weight:23},
+  'K2': {value:"角型２号", long:332, short:240, thickness:1, weight:18},
+  'K20': {value:"角型20号", long:324, short:229, thickness:1, weight:16},
+  'K3': {value:"角型３号", long:277, short:216, thickness:1, weight:14},
+  'K4': {value:"角型４号", long:267, short:197, thickness:1, weight:12},
+  'K5': {value:"角型５号", long:240, short:190, thickness:1, weight:10},
+  'K6': {value:"角型６号", long:229, short:162, thickness:1, weight:9},
+  'K7': {value:"角型７号", long:205, short:142, thickness:1, weight:7},
+  'K8': {value:"角型８号", long:197, short:119, thickness:1, weight:6},
+  'N1': {value:"長型１号", long:332, short:142, thickness:1, weight:10},
+  'N2': {value:"長型２号", long:277, short:119, thickness:1, weight:8},
+  'N3': {value:"長型３号", long:235, short:120, thickness:1, weight:7},
+  'N13': {value:"長型13号", long:235, short:105, thickness:1, weight:6},
+  'N30': {value:"長型30号", long:235, short:92, thickness:1, weight:6},
+  'N4': {value:"長型４号", long:205, short:90, thickness:1, weight:5},
+  'N40': {value:"長型40号", long:225, short:90, thickness:1, weight:5},
+}   
+
+// --- 郵便種別メッセージ（現在未使用）
+const postalClassMessage = [
+  {id:'calculating', message:'計算中です・・・'},
+  {id:'standard', message:'定形郵便物'},
+  {id:'nonstandard', message:'定形外・規格内郵便物'},
+  {id:'extra', message:'定形外・規格外郵便物'},
+  {id:'parcel', message:'郵便小包での扱いとなります'},
+]
+
+// ----- 各郵便種別ごとの料金表（重量何ｇまで,料金）
+// --- !!! 郵便料金が変更となった場合はここを修正 !!! ---
+// --- 定形郵便物
+const standardPostageArray = [
+  { weightLimit:25, fee:84 },
+  { weightLimit:50, fee:94 },
+];
+
+// --- 定形外・規格内郵便物
+const nonstandardPostageArray = [
+  { weightLimit:50, fee:120 },
+  { weightLimit:100, fee:140 },
+  { weightLimit:150, fee:210 },
+  { weightLimit:250, fee:250 },
+  { weightLimit:500, fee:390 },
+  { weightLimit:1000, fee:580 },
+];
+
+// --- 定形外・規格外郵便物
+const extraPostageArray = [
+  { weightLimit:50, fee:200 },
+  { weightLimit:100, fee:220 },
+  { weightLimit:150, fee:300 },
+  { weightLimit:250, fee:350 },
+  { weightLimit:500, fee:510 },
+  { weightLimit:1000, fee:710 },
+  { weightLimit:2000, fee:1040 },
+  { weightLimit:4000, fee:1350 },
+];  
+
+
+// // 点滅表示
+// function flashOff(targetElement){
+//   const blinkTarget = targetElement;
+//   blinkTarget.classList.add('hidden');
+// }
+
+// function flashOn(targetElement){
+//   const blinkTarget = targetElement;
+//   blinkTarget.classList.remove('hidden');
+// }
+
+// アコーディオンメニュー（現在未使用）
+const acbtns = document.querySelectorAll(".accordion-btn");
+
+acbtns.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    const panel = this.nextElementSibling;
+
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+});
+
