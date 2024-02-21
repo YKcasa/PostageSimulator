4f3e910023718e63787b90dfc9a98dbe90390446
YKcasa <131603850+YKcasa@users.noreply.github.com>
2024-02-21 19:49:10 +0900

    temp

--- scripts/main.js
+++ scripts/main.js
@@ -264,31 +264,8 @@ function updateDisplay(area, contents, delay = 0){
     area.textContent = `${contents}`;
 }
 
-function turnPostageCounter(previousValue = 0, targetValue){
-
-    // let displayValue = previousValue;
-
-    // const turnCounter = () => {
-    //     displayPostage.textContent = displayValue;
-    //     displayValue++
-
-    //     if ( > ){
-
-    //     }
-    // }
-
-    // if (previousValue <= targetValue){
-    //     const difference = targetValue - previousValue;
-    //     const speed = 600 / difference;
-    //     clearInterval(counterData);
-    //     counterData = setInterval(turnCounter, speed);
-    // }else{
-    //     const difference = previousValue - targetValue;
-    //     const speed = 600 / difference;
-    //     clearInterval(counterData);
-    //     counterData = setInterval(turnCounter, speed);
-
-    // }
+// --- 郵便料金の値が変更するとき、カウントアップ・カウントダウン表示する（作成中）
+function turnPostageCounter(){
 }
 
 
